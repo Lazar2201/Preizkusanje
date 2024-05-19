cff635a5dc7984ac0c349c82b483f57a47bc58e0
Laki2201 <lazarlakikv5@gmail.com>
2024-05-19 20:49:41 +0200

    Prepravljeno

--- cmake-build-debug/BellmanFord[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/Lazar/CLionProjects/BF/cmake-build-debug/BellmanFord[1]_tests.cmake")
-  include("C:/Users/Lazar/CLionProjects/BF/cmake-build-debug/BellmanFord[1]_tests.cmake")
-else()
-  add_test(BellmanFord_NOT_BUILT BellmanFord_NOT_BUILT)
-endif()
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Lazar/CLionProjects/BF
-# Build directory: C:/Users/Lazar/CLionProjects/BF/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Lazar/CLionProjects/BF/cmake-build-debug/BellmanFord[1]_include.cmake")
-subdirs("_deps/googletest-build")
